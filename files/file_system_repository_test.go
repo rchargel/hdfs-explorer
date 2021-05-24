28f2b82c0bb4813b091eccf6b7014a7eb4cd453f
rchargel <rchargel@zcarioca.net>
2021-05-24 16:28:14 -0400

    Cleaning up test code

--- files/file_system_repository_test.go
+++ files/file_system_repository_test.go
@@ -7,64 +7,78 @@ import (
 )
 
 func validateLength(t *testing.T, fsr FileSystemRepository, expectedLength uint8) {
-	if fsList, e := fsr.List(); e != nil {
+	fsList, e := fsr.List()
+	if e != nil {
 		t.Errorf("Could not get listing: %v", e.Error())
-	} else {
-		if len(fsList) != int(expectedLength) {
-			t.Errorf("Should have a length of %d but was %d", expectedLength, len(fsList))
-		}
+	}
+	if len(fsList) != int(expectedLength) {
+		t.Errorf("Should have a length of %d but was %d", expectedLength, len(fsList))
 	}
 }
 
-func TestFileSystemRepository(t *testing.T) {
-	if file, err := ioutil.TempFile("", "file*.repo"); err == nil {
-		path := file.Name()
+func validateRecordFound(t *testing.T, fsr FileSystemRepository) {
+	fs, err := fsr.FindByName("My FS")
+	if err != nil {
+		t.Errorf("Unable to find record: %v", err.Error())
+		t.Fail()
+	}
+
+	if fs.Description != "This is a description" {
+		t.Errorf("Invalid description: %v", fs.Description)
+		t.Fail()
+	}
+}
+
+func createTestFile(t *testing.T) string {
+	file, err := ioutil.TempFile("", "file*.repo")
+	var path string
+	if err == nil {
+		path = file.Name()
 		file.Close()
 		os.Remove(file.Name())
-
-		if fsr, e := GetFileSystemRepositoryFromPath(path); e == nil {
-			validateLength(t, fsr, 0)
-
-			if b := fsr.Store(FileSystem{
-				Name:        "My FS",
-				Description: "This is a description",
-				Addresses:   []string{"localhost:9000"},
-			}); b != nil {
-				t.Errorf("Could not store record: %v", b.Error())
-			}
-
-			validateLength(t, fsr, 1)
-
-			if value, el := fsr.FindByName("My FS"); el != nil {
-				t.Errorf("Could not find value in FileSystemRepository: %v", el.Error())
-			} else if value.Description != "This is a description" {
-				t.Error("Found an incorrect value")
-			}
-		} else {
-			t.Errorf("Error creating File System: %v", e.Error())
-		}
-
-		if fsr, e := GetFileSystemRepositoryFromPath(path); e == nil {
-			validateLength(t, fsr, 1)
-
-			if value, b := fsr.FindByName("My FS"); b != nil {
-				t.Error("Could not find value in FileSystemRepository")
-			} else if value.Description != "This is a description" {
-				t.Error("Found an incorrect value")
-			}
-
-			if b := fsr.Remove("My FS"); b != nil {
-				t.Errorf("Error removing record: %v", b.Error())
-			}
-		} else {
-			t.Errorf("Error creating File System: %v", e.Error())
-		}
-		if fsr, e := GetFileSystemRepositoryFromPath(path); e == nil {
-			validateLength(t, fsr, 0)
-		} else {
-			t.Errorf("Error creating File System: %v", e.Error())
-		}
-	} else {
-		t.Errorf("Error when creating temp file: %v", err.Error())
+		return path
 	}
+	t.Error("Unable to create temp file")
+	t.Fail()
+	return path
+}
+
+func getRepository(t *testing.T, path string) FileSystemRepository {
+	fsr, err := GetFileSystemRepositoryFromPath(path)
+	if err != nil {
+		t.Errorf("Unable to build file system repository: %v", err.Error())
+		t.Fail()
+	}
+	return fsr
+}
+
+func TestFileSystemRepository(t *testing.T) {
+	path := createTestFile(t)
+	fsr := getRepository(t, path)
+
+	validateLength(t, fsr, 0)
+
+	if b := fsr.Store(FileSystem{
+		Name:        "My FS",
+		Description: "This is a description",
+		Addresses:   []string{"localhost:9000"},
+	}); b != nil {
+		t.Errorf("Could not store record: %v", b.Error())
+	}
+
+	validateLength(t, fsr, 1)
+	validateRecordFound(t, fsr)
+
+	// re-initialize
+	fsr = getRepository(t, path)
+	validateLength(t, fsr, 1)
+	validateRecordFound(t, fsr)
+
+	if b := fsr.Remove("My FS"); b != nil {
+		t.Errorf("Error removing record: %v", b.Error())
+	}
+
+	// re-initialize
+	fsr = getRepository(t, path)
+	validateLength(t, fsr, 0)
 }
