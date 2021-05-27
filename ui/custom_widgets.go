375dae527a63d1472e3400adfa23feb0e8a9b9a5
rchargel <rchargel@zcarioca.net>
2021-05-27 12:18:33 -0400

    Adding custom widgets

---
+++ ui/custom_widgets.go
@@ -0,0 +1,101 @@
+package ui
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/widget"
+)
+
+type EventListener func(event interface{})
+
+type ListWithListener struct {
+	list     *widget.List
+	data     binding.StringList
+	selected int
+
+	listeners []EventListener
+}
+
+type ClickableLabel struct {
+	widget.Label
+
+	listeners []EventListener
+}
+
+func NewClickableLabel(text string) *ClickableLabel {
+	label := &ClickableLabel{}
+	label.ExtendBaseWidget(label)
+	label.SetText(text)
+	label.listeners = make([]EventListener, 0)
+	return label
+}
+
+func (l *ClickableLabel) AddListener(listener EventListener) {
+	l.listeners = append(l.listeners, listener)
+}
+
+func (l *ClickableLabel) Tapped(_ *fyne.PointEvent) {
+	for _, listener := range l.listeners {
+		listener(l.Text)
+	}
+}
+
+func NewSelectableList(data binding.StringList) *ListWithListener {
+	var list *ListWithListener
+	list = &ListWithListener{widget.NewListWithData(
+		data,
+		func() fyne.CanvasObject {
+			clickableLabel := NewClickableLabel("template")
+			clickableLabel.AddListener(list.labelClick)
+			return clickableLabel
+		},
+		func(di binding.DataItem, co fyne.CanvasObject) {
+			co.(*ClickableLabel).Bind(di.(binding.String))
+		},
+	), data, -1, make([]EventListener, 0)}
+	return list
+}
+
+func (l *ListWithListener) labelClick(event interface{}) {
+	label := event.(string)
+
+	idx := l.getIndex(label)
+	if idx > -1 {
+		l.list.Select(idx)
+		if l.selected != idx {
+			l.selected = idx
+			for _, listener := range l.listeners {
+				listener(label)
+			}
+		}
+	} else {
+		l.list.Unselect(l.selected)
+		l.selected = -1
+	}
+}
+
+func (l *ListWithListener) getIndex(text string) int {
+	values, _ := l.data.Get()
+
+	for i, value := range values {
+		if value == text {
+			return i
+		}
+	}
+	return -1
+}
+
+func (l *ListWithListener) CanvasObject() fyne.CanvasObject {
+	return l.list
+}
+
+func (l *ListWithListener) AddListener(listener EventListener) {
+	l.listeners = append(l.listeners, listener)
+}
+
+func (l *ListWithListener) ClearSelected() {
+	if l.selected > -1 {
+		l.list.Unselect(l.selected)
+		l.selected = -1
+	}
+}
--- ui/file_repo_manager.go
+++ ui/file_repo_manager.go
@@ -1,11 +1,14 @@
 package ui
 
 import (
+	"errors"
 	"fmt"
+	"regexp"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 	"github.com/rchargel/hdfs-explorer/files"
@@ -15,8 +18,9 @@ import (
 type FileRepoManagerDialog struct {
 	Title          string
 	popup          *widget.PopUp
+	listUI         *ListWithListener
 	fileSystemRepo files.FileSystemRepository
-	fsList         binding.DataList
+	fsList         binding.StringList
 	name           binding.String
 	description    binding.String
 	addresses      binding.String
@@ -28,19 +32,54 @@ func NewFileRepoManagerDialog() *FileRepoManagerDialog {
 	if err != nil {
 		log.Error.Fatal(err)
 	}
-	dialog := &FileRepoManagerDialog{}
-	dialog.Title = "Connections"
-	dialog.fileSystemRepo = repository
-	fileSystems := binding.BindStringList(dialog.fileSystemNames())
-	dialog.fsList = fileSystems
-	dialog.name = binding.BindString(nil)
-	dialog.description = binding.BindString(nil)
-	dialog.addresses = binding.BindString(nil)
+	dialog := &FileRepoManagerDialog{
+		Title:          "Connections",
+		popup:          nil,
+		fileSystemRepo: repository,
+		fsList:         binding.NewStringList(),
+		name:           binding.NewString(),
+		description:    binding.NewString(),
+		addresses:      binding.NewString(),
+	}
+
+	dialog.fsList.Set(dialog.fileSystemNames())
 
 	return dialog
 }
 
-func (d *FileRepoManagerDialog) fileSystemNames() *[]string {
+func (d *FileRepoManagerDialog) addNew() {
+	d.listUI.ClearSelected()
+	d.name.Set("")
+	d.description.Set("")
+	d.addresses.Set("")
+}
+
+func (d *FileRepoManagerDialog) save() error {
+	name, _ := d.name.Get()
+	desc, _ := d.description.Get()
+	addr, _ := d.addresses.Get()
+
+	re := regexp.MustCompile(`\s+`)
+	addresses := re.FindAllString(addr, -1)
+
+	if len(name) == 0 || len(desc) == 0 || addresses == nil {
+		return errors.New("Missing field data")
+	}
+	fs := files.FileSystem{
+		Name:        name,
+		Description: desc,
+		Addresses:   addresses,
+	}
+
+	err := d.fileSystemRepo.Store(fs)
+	if err == nil {
+		d.fsList.Set(d.fileSystemNames())
+	}
+	d.addNew()
+	return err
+}
+
+func (d *FileRepoManagerDialog) fileSystemNames() []string {
 	list, err := d.fileSystemRepo.List()
 	if err != nil {
 		ShowFatalError(err)
@@ -49,35 +88,40 @@ func (d *FileRepoManagerDialog) fileSystemNames() *[]string {
 	for i, fileSystem := range list {
 		names[i] = fileSystem.Name
 	}
-	return &names
+	return names
 }
 
-func (f *FileRepoManagerDialog) Open() {
-	if f.popup == nil {
-		fsList := f.fileSystemList()
-		form := f.createForm()
+func (d *FileRepoManagerDialog) Open() {
+	if d.popup == nil {
+		fsList := d.fileSystemList()
+		form := d.createForm()
 		content := container.NewHBox(fsList, form)
 		newButton := widget.NewButton("New", func() {
-			val := fmt.Sprintf("Next %d", f.fsList.Length()+1)
-			f.fsList.(binding.ExternalStringList).Append(val)
+			d.addNew()
 		})
-		f.popup = NewCustomDialog(
-			f.Title,
+		saveButton := widget.NewButton("Save", func() {
+			if err := d.save(); err != nil {
+				dialog.ShowError(err, Window)
+			}
+		})
+		d.popup = NewCustomDialog(
+			d.Title,
 			content,
 			newButton,
+			saveButton,
 		)
 	}
-	f.popup.Show()
+	d.popup.Show()
 }
 
-func (f *FileRepoManagerDialog) createForm() *fyne.Container {
+func (d *FileRepoManagerDialog) createForm() *fyne.Container {
 	nameLabel := widget.NewLabel("Name: ")
-	nameEntry := widget.NewEntryWithData(f.name)
+	nameEntry := widget.NewEntryWithData(d.name)
 	descriptionLabel := widget.NewLabel("Description: ")
-	descriptionEntry := widget.NewEntryWithData(f.description)
+	descriptionEntry := widget.NewEntryWithData(d.description)
 	descriptionEntry.MultiLine = true
 	addressLabel := widget.NewLabel("Addresses (new line sperated):")
-	addressEntry := widget.NewEntryWithData(f.addresses)
+	addressEntry := widget.NewEntryWithData(d.addresses)
 	addressEntry.MultiLine = true
 
 	return container.NewVBox(
@@ -90,15 +134,13 @@ func (f *FileRepoManagerDialog) createForm() *fyne.Container {
 	)
 }
 
-func (f *FileRepoManagerDialog) fileSystemList() *fyne.Container {
-	list := widget.NewListWithData(
-		f.fsList,
-		func() fyne.CanvasObject {
-			return widget.NewLabel("Connections")
-		},
-		func(i binding.DataItem, o fyne.CanvasObject) {
-			o.(*widget.Label).Bind(i.(binding.String))
-		},
-	)
-	return container.New(layout.NewMaxLayout(), list)
+func (d *FileRepoManagerDialog) fileSystemList() *fyne.Container {
+	d.listUI = NewSelectableList(d.fsList)
+
+	d.listUI.AddListener(func(event interface{}) {
+		label := event.(string)
+		fmt.Printf("Selected label %v\n", label)
+	})
+
+	return container.New(layout.NewMaxLayout(), d.listUI.CanvasObject())
 }
